b166e41857c59873dc0245ba113366265f094af3
Jihee <starlover27@naver.com>
2021-07-31 18:30:29 +0900

    Jihee's first commit

---
+++ src/main/java/gangnyang/mohmsee/MohmseeApplication.java
@@ -0,0 +1,13 @@
+package gangnyang.mohmsee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohmseeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohmseeApplication.class, args);
+	}
+
+}
